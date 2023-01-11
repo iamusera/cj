d39ebc48e1f063506bda663a81a8f5e3c04a85d4
iamusera <liuzy@kaiyufound.com>
2023-01-11 10:47:43 +0800

    init

--- storage/src/main/java/com/kaiyu/storage/proc/APP.java
+++ storage/src/main/java/com/kaiyu/storage/proc/APP.java
@@ -1,13 +1,13 @@
-package com.kaiyu.server;
+package com.kaiyu.storage.proc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class APP {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(APP.class, args);
     }
 
 }
