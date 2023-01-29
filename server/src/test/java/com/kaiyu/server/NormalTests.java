40cefe27a1cf0295aa9aa06b7ebcefa948abc4d5
iamusera <liuzy@kaiyufound.com>
2023-01-29 10:15:49 +0800

    init

---
+++ server/src/test/java/com/kaiyu/server/NormalTests.java
@@ -0,0 +1,9 @@
+package com.kaiyu.server;
+
+public class NormalTests {
+    public NormalTests(){};
+
+    public static void main(String[] args) {
+        System.out.println(NormalTests.class);
+    }
+}
